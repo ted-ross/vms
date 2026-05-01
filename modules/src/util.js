8d1dd9fda12a7e37c6e41b2c79ff1518060bd89e
Fernando Giorgetti <fgiorget@redhat.com>
2026-05-01 18:01:56 -0300

    Fixed regex used to normalize timestamp fields

--- modules/src/util.js
+++ modules/src/util.js
@@ -79,7 +79,7 @@ const uuidRegex = RegExp(
 )
 const dnsRegex = RegExp("^[A-Za-z][A-Za-z0-9-\.]{0,63}$")
 const dtzRegex = RegExp(
-  "^[1-2][0-9]{3}-[0-1][0-9]-[0-5][0-9]T[0-2][0-9]:[0-5][0-9]$",
+  "^[1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]{3}Z$",
 )
 
 export function IsValidUuid(text) {
